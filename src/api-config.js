e3389117b2e9fc0dd2a882c4225235e9adb7ca0c
gabriel vareilles <gabriel.vareilles@gmail.com>
2018-03-29 16:47:29 +0200

    Better backend host condition

--- src/api-config.js
+++ src/api-config.js
@@ -2,12 +2,10 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === 'medico-react.herokuapp.com') {
+if (hostname === 'localhost') {
+  backendHost = 'http://localhost:4000';
+} else {
   backendHost = 'https://medico-gql-api.herokuapp.com';
 }
 
-if (hostname === 'localhost') {
-  backendHost = 'http://localhost:4000';
-}
-console.log(hostname);
 export const API_ROOT = backendHost;
